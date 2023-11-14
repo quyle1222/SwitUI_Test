8c917b69e4ccd54f3a48a4bfc5aabe1086d6ddb0
nhutbm <nhutbm@rikkeisoft.com>
2023-11-14 14:35:04 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by TuPT on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
